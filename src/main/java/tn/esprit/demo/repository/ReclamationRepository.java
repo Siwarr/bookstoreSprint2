55a73b766a939849b00ad6851214629beb953bd7
M-Jendli <mehdi.jendli@esprit.tn>
2021-02-05 22:47:15 +0100

    Reclamation Service Impl commit

--- src/main/java/tn/esprit/demo/repository/ReclamationRepository.java
+++ src/main/java/tn/esprit/demo/repository/ReclamationRepository.java
@@ -1,11 +1,36 @@
 package tn.esprit.demo.repository;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import java.util.Date;
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import tn.esprit.demo.entities.Reclamation;
+import tn.esprit.demo.entities.User;
 
 @Repository
 public interface ReclamationRepository extends CrudRepository <Reclamation,Long>{
 
+	List<Reclamation> findAllByOrderByDateDesc(Pageable page);
+	List<Reclamation> findByUserOrderByDateDesc(User u);
+	List<Reclamation> findByDate(Date date);
+	List<Reclamation> findByDateBetween(Date date1,Date date2);
+	List<Reclamation> findByTitreContainingOrderByDateDesc(String txt);
+	List<Reclamation> findTop3ByUserOrderByDateDesc(User u);
+	
+	@Query("SELECT COUNT(r) FROM Reclamation r WHERE r.user=:user")
+	long countByUser(@Param("user")User u);
+     
+	List<Reclamation> findByUserInOrderByDateDesc(List <User> users);
+	
+	//Another Method
+	//Named parameters
+	@Query("SELECT r FROM Reclamation r WHERE r.user.nom LIKE %:nom%")
+	List<Reclamation> searchByUserName(@Param("nom")String nom);
+	
+	//deletebyUser
+	List<Reclamation> removeByUser(User u);
 }
--- src/main/java/tn/esprit/demo/service/IReclamationService.java
+++ src/main/java/tn/esprit/demo/service/IReclamationService.java
@@ -1,5 +1,6 @@
 package tn.esprit.demo.service;
 
+import java.text.ParseException;
 import java.util.List;
 
 import tn.esprit.demo.entities.Reclamation;
@@ -8,11 +9,21 @@ import tn.esprit.demo.entities.Reclamation;
 public interface IReclamationService {
 	
     List<Reclamation> retrieveAllReclamations();
-	
 	Reclamation addReclamation(Reclamation r);
 	void deleteReclamation (String id);
 	Reclamation updateReclamation (Reclamation r);
 	Reclamation retrieveReclamation (String id);
-
-
+	
+	List<Reclamation> findReclamationByUser(String id);
+	List<Reclamation> findReclamationByDate(String date) throws ParseException;
+	List<Reclamation> findReclamationByDateBetween(String date1,String date2) throws ParseException;
+	List<Reclamation> findReclamationByTitleContaining(String txt);
+	
+	List<Reclamation> findReclamationByUserNameLike(String nom);
+	List<Reclamation> deleteReclamationByUser(String id);
+	
+	List<Reclamation> retrieveAllReclamationsLimit3();
+	List<Reclamation> findReclamationByUserLimit(String id);
+	long countReclamation();
+	long countReclamationByUser(String idUser);
 }
