c70fd17e3e10134b2c3280707ed5500f4055045e
amal naamoun <naamounamal@gmail.com>
2021-02-12 01:14:23 +0100

    c

--- src/main/java/tn/esprit/demo/service/CommandeServiceImpl.java
+++ src/main/java/tn/esprit/demo/service/CommandeServiceImpl.java
@@ -3,7 +3,7 @@ package tn.esprit.demo.service;
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+//import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
